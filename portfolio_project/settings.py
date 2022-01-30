c1720cc7df69219cb43eccea94697b1ea7b7153f
AnoMorCH <anomorch@gmail.com>
2022-01-30 10:01:39 +0300

    Connection to Heroku (process)

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -31,7 +31,10 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1:8000',
+    'anomorch-portfolio-project.herokuapp.com',
+]
 
 
 # Application definition
